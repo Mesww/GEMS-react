e470540fc131e5de197e1d1c185f5587fe62d1f1
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-07-14 18:15:50 +0700

    แก้ให้เป็นfunction คำนวน เวลาและรถบัสที่ใกล้ที่สุด

--- src/components/map/page.tsx
+++ src/components/map/page.tsx
@@ -1,40 +1,10 @@
-import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import MapComponant from "./mapComponent";
 import Navbar from "../navbar/navbar";
-import ClosestBus from "../calulateDistance/userAndBusDistance";
-import { useWebSocketData } from "../../containers/getGemsDataWebsocket/getGemsWebsocket";
-import useUserLocation from "../../containers/userLocation/getUserLocation";
 
-interface TrackerData {
-  server_time: string;
-  tracker_time: string;
-  direction: number;
-  position: string;
-  speed: number;
-}
-
-interface WebSocketMessage {
-  status: string;
-  data: {
-    [key: string]: TrackerData;
-  };
-}
 
 const Mappage = () => {
-  // get userlocation ===================================================
-  const location = useUserLocation();
-
-// รับข้อมูลจาก websocket  ================================================
-const { messages } = useWebSocketData() as {
-  messages: WebSocketMessage | null;
-};
-
-
-const data = useMemo(() => {
-  return messages && messages.status === "ok" ? messages.data : null;
-}, [messages]);
   const [cookies, setCookie] = useCookies(["token"]);
   const navigate = useNavigate();
   const handleSignout = () => {
@@ -46,10 +16,10 @@ const data = useMemo(() => {
     }
   };
 
-  
+
+
   return (
     <>
-      <ClosestBus userLocation={location} busData={data} />
       <Navbar />
       <MapComponant />
     </>
