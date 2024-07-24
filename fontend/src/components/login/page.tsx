ba23d5a1700c21270eeaaed2927b876aa3dc1642
Mesww <6431501102@lamduan.mfu.ac.th>
2024-07-25 00:04:25 +0700

    set loading login

--- fontend/src/components/login/page.tsx
+++ fontend/src/components/login/page.tsx
@@ -1,64 +1,87 @@
 import logo from "/Screenshot_2567-07-10_at_12.04.25-removebg.png";
-import mfulogo from '/Mae-Fah-Luang-University-2-768x779.png';
+import mfulogo from "/Mae-Fah-Luang-University-2-768x779.png";
 import googlelogo from "/googlelogo.png";
 import "./style.sass";
-import { getUserinfo, sencodetobackend } from '../../containers/login/Login';
-import { useNavigate} from 'react-router-dom';
-import {  useGoogleLogin} from '@react-oauth/google';
+import { getUserinfo, sencodetobackend } from "../../containers/login/Login";
+import { useNavigate } from "react-router-dom";
+import { useGoogleLogin } from "@react-oauth/google";
 import { useCookies } from "react-cookie";
+import { useState } from "react";
 // import { Button,TouchableHighlight,View,Icon,Text } from "react-native";
-const Login = () => { 
-    const [, setCookie] = useCookies(['token']);
-    const navigate = useNavigate();
-    const login = async (codeResponse: { code: string; })=>{
-       const token = await sencodetobackend(codeResponse.code);
-        setCookie("token",token);
-        // get role from user info 
-        const userInfo = await getUserinfo(token);
-        
-        if (userInfo.role=== null ) {
-           return console.error("Database error");
-        }
+const Login = () => {
+  const [, setCookie] = useCookies(["token"]);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const login = async (codeResponse: { code: string }) => {
+    setLoading(true);
+    const token = await sencodetobackend(codeResponse.code);
+    setCookie("token", token);
+    // get role from user info
+    const userInfo = await getUserinfo(token);
 
-        console.log("Role : ",userInfo.role);
-        
-        switch (userInfo.role) {
-            case "USER":
-                navigate('/map',{replace:true})
-                break;
-                case "ADMIN":
-                navigate('/map',{replace:true})
-                break;
-            default:
-                break;
-        }
+    if (userInfo.role === null) {
+      return console.error("Database error");
     }
 
-    const auth = useGoogleLogin({
-        onSuccess: codeResponse => login(codeResponse),
-        onError: () => {
-          console.log('Login Failed');
-        },
-        flow: 'auth-code',
-      }
-    );
- 
-    return <>
-        <div className=" grid md:grid-cols-3 grid-rows-3  w-screen h-screen ">
-            <div className="flex ml-5 mt-5 items-center h-14">
-                <img src={mfulogo} alt="mfulogo" className="w-14 h-14" />
-                <h1 className="texthighlightcolor">MFU</h1>
-            </div>
-            <div className="md:col-start-2 row-start-2  md:justify-self-center self-center">
-                <h1 className=" text-center mb-4 textcolor text-3xl font-bold ">GEMS</h1>
-                <img src={logo} alt="mfu-logo" className=" lg:w-96 md:w-80 w-64 mb-5 mx-auto" />
-               <div className="flex justify-center googlebutton h-14 items-center rounded-full" onClick={auth}>
-                      <img src={googlelogo} alt="googlelogo" width={20}  />
-                      <p className=" text-white ml-2"> Sign in</p>
-               </div>
-                           {/* <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> */}
-            </div>
+    console.log("Role : ", userInfo.role);
+    setLoading(false);
+    switch (userInfo.role) {
+      case "USER":
+        navigate("/map", { replace: true });
+        break;
+      case "ADMIN":
+        navigate("/map", { replace: true });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const auth = useGoogleLogin({
+    onSuccess: (codeResponse) => login(codeResponse),
+    onError: () => {
+      console.log("Login Failed");
+    },
+    flow: "auth-code",
+  });
+
+  return (
+    <>
+        {/* loading */}
+      {loading && (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-50 flex justify-center items-center">
+          <div className="loader">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
+      )}
+      <div className=" grid md:grid-cols-3 grid-rows-3  w-screen h-screen ">
+        <div className="flex ml-5 mt-5 items-center h-14">
+          <img src={mfulogo} alt="mfulogo" className="w-14 h-14" />
+          <h1 className="texthighlightcolor">MFU</h1>
+        </div>
+        <div className="md:col-start-2 row-start-2  md:justify-self-center self-center">
+          <h1 className=" text-center mb-4 textcolor text-3xl font-bold ">
+            GEMS
+          </h1>
+          <img
+            src={logo}
+            alt="mfu-logo"
+            className=" lg:w-96 md:w-80 w-64 mb-5 mx-auto"
+          />
+          <div
+            className="flex justify-center googlebutton h-14 items-center rounded-full"
+            onClick={auth}
+          >
+            <img src={googlelogo} alt="googlelogo" width={20} />
+            <p className=" text-white ml-2"> Sign in</p>
+          </div>
+          {/* <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> */}
+        </div>
+      </div>
     </>
-}
+  );
+};
 export default Login;
