655bdea30853b2b3f7c9024168fce87905e3f38f
Nathan Boonkon <158584121+NathanB054@users.noreply.github.com>
2024-08-06 15:35:25 +0700

    update activity api เพิ่ม destinationMarker

--- Server-api-temp/src/models/activity_model.ts
+++ Server-api-temp/src/models/activity_model.ts
@@ -3,18 +3,20 @@ import mongoose, { Collection } from "mongoose";
 interface activities {
     email: string;
     location: string;
-    marker: string;
+    stationMarker: string;
     time: Date;
     route: string;
+    destinationMarker: string;
   }
 
 const activitySchema = new mongoose.Schema(
     {
       email: { type: String, required: false},
       location: { type: String, required: true },
-      marker: { type: String, required: true },
+      stationMarker: { type: String, required: true },
       time: { type: Date, required: false },
       route: { type: String, required: true },
+      destinationMarker: { type: String, required: false },
     },
     { collection: "activities" }
   );
@@ -22,9 +24,10 @@ const activitySchema = new mongoose.Schema(
   interface activityDocument extends mongoose.Document {
     email: string;
     location: string;
-    marker: string;
+    stationMarker: string;
     time: Date;
     route: string;
+    destinationMarker: string;
     set(x: activities): this; // Define `set` as an instance method
   }
 
@@ -34,9 +37,10 @@ const activitySchema = new mongoose.Schema(
 Activity.prototype.set = function (x: activities) {
     this.studentid = x.email;
     this.location = x.location;
-    this.marker = x.marker;
+    this.stationMarker = x.stationMarker;
     this.time = x.time;
     this.route = x.route;
+    this.destinationMarker = x.destinationMarker;
     return this.save(); // Save the document to the database
   };
 
